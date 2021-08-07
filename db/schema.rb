abc87371e86412ee091cfe258a91712a26eb6f93
pedro <pedropmancillac@gmail.com>
2021-08-07 12:06:45 -0400

    nueva columna a productos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_163824) do
+ActiveRecord::Schema.define(version: 2021_08_07_160258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,20 @@ ActiveRecord::Schema.define(version: 2021_08_06_163824) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "stock"
+    t.integer "price"
+    t.bigint "category_id", null: false
+    t.bigint "suplier_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "url_img"
+    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["suplier_id"], name: "index_products_on_suplier_id"
   end
 
   create_table "supliers", force: :cascade do |t|
@@ -55,4 +69,6 @@ ActiveRecord::Schema.define(version: 2021_08_06_163824) do
   end
 
   add_foreign_key "orders", "users"
+  add_foreign_key "products", "categories"
+  add_foreign_key "products", "supliers"
 end

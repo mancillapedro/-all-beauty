6524199e661c295ee36649166fdb2f71ced4a5dc
pedro <pedropmancillac@gmail.com>
2021-08-07 13:42:44 -0400

    seed products, suplier, categories listo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,41 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 
+# ----- inicio creacion supliers -----
+
 supliers = JSON.parse(File.read('db/json_seed/supliers.json'))
 
 supliers.each do |suplier|
-    Suplier.create(suplier)
+    new_suplier = Suplier.new(suplier)
+    new_suplier.save!
+    puts "suplier #{new_suplier.name} creado"
 end
+
+supliers = Suplier.all
+
+# ----- fin creacion suppliers -----
+
+# ----- inicio creacion categories y products -----
+
+products = JSON.parse(File.read('db/json_seed/products.json'))
+
+products.each do |cat|
+  # --- creacion categories ---
+  category =  Category.new(name: cat["category"])
+  category.save!
+  puts "categoria #{category.name} creada"
+  cat["products"].each do |product|
+    # --- creacion products ---
+    product["stock"] = rand(0..30)
+    new_product = Product.new(product)
+    new_product.category = category
+    new_product.suplier = supliers.sample
+    new_product.save!
+    puts "producto #{new_product.name} creado"
+  end
+end
+
+# ----- fin creacion productos y categorias -----
+
+puts "fin"
+

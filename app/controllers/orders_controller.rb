89c5be06bf4618ae28d32ce3de3fd68d9717a956
pilar garcia <pilargarciaroyo@gmail.com>
2021-08-09 11:25:24 -0400

    cambio de metodos en controller orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,38 +1,17 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: [:show, :destroy, #:edit, :update]
-
-  def index
-    @orders = Order.all
-  end    
-
   def show
-  end
-
-  def new
-    @order = Order.new
-  end
-
-  def create
-    order = Order.new(task_params)
-    order.save
-    redirect_to product_orders
-  end
-
-  # def edit
-  # end
-
-  # def update
-   # @task.update(order_params)
-   # redirect_to order_path(@order)
-  # end
-
-  def destroy
-    @order.destroy
-    redirect_to orders_path
-  end
-    
-  def set_order
     @order = Order.find(params[:id])
   end
 
+  def create
+    order = Order.new(order_params)
+    order.save
+    redirect_to orders
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    redirect_to product_orders_path
+  end
 end

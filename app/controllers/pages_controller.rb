1bca5a697bed12eb4f89c1f73af58c92afa86779
pedro <pedropmancillac@gmail.com>
2021-08-14 11:46:44 -0400

    logica navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :products, :supliers]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
   end

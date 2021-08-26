a04f4282f4fd4250398129c7ea2d255788d50e1f
pedro <pedropmancillac@gmail.com>
2021-08-26 15:35:43 -0400

    association user suplier

--- app/models/suplier.rb
+++ app/models/suplier.rb
@@ -1,3 +1,5 @@
 class Suplier < ApplicationRecord
   has_many :products
+  has_one :suplier_user_suplier
+  has_one :user_suplier, through: :suplier_user_suplier
 end
--- app/models/user_suplier.rb
+++ app/models/user_suplier.rb
@@ -3,4 +3,6 @@ class UserSuplier < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one :suplier_user_suplier
+  has_one :suplier, through: :suplier_user_suplier
 end

78f14c51b7b1d04199b91ad980295529110dd672
pedro <pedropmancillac@gmail.com>
2021-08-26 15:23:52 -0400

    join tabla suplier_user_suplier

---
+++ app/models/suplier_user_suplier.rb
@@ -0,0 +1,4 @@
+class SuplierUserSuplier < ApplicationRecord
+  belongs_to :suplier
+  belongs_to :user_suplier
+end
---
+++ test/models/suplier_user_suplier_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SuplierUserSuplierTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

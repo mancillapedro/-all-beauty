31235fd096e33eaa5585c2b0f6684d811b78bf1d
pedro <pedropmancillac@gmail.com>
2021-08-26 17:35:23 -0400

    skip home about

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :about]
+  skip_before_action :authenticate_user_suplier!, only: [:home, :about]
 
   def home
-    @supliers=Suplier.all
+    @supliers = Suplier.all
   end
 
   def about; end

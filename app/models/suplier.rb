6c026f767360c9099fbc5b176c7f3740f40ac8f9
pedro <pedropmancillac@gmail.com>
2021-08-30 18:57:17 -0400

    modelo suplier image attach

--- app/models/suplier.rb
+++ app/models/suplier.rb
@@ -3,4 +3,5 @@ class Suplier < ApplicationRecord
   has_one :suplier_user_suplier
   has_one :user_suplier, through: :suplier_user_suplier
   has_many :suplier_product_orders
+  has_one_attached :image
 end

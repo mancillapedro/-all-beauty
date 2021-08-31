3a61cb603d40148e801416a5dbcb92b9c051c4f6
pedro <pedropmancillac@gmail.com>
2021-08-30 21:44:47 -0400

    modificaciones imagenes attach+ç

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,10 @@ class Product < ApplicationRecord
   belongs_to :suplier
   has_many :product_orders
   has_one_attached :image
+
+  def valid_img(attr)
+    return "<%= cl_image_tag '#{image.key}', #{attr}%>" if image.attached?
+
+    return "<%= image_tag '#{url_img}', #{attr} %>"
+  end
 end
